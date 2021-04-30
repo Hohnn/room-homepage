2c9afd0b69cdcb1ed76d99b75662aa18a9cabeb8
Hohnn <jean-bateee@hotmail.fr>
2021-05-01 00:21:12 +0200

    add event

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,50 @@
 const arrowLeft = document.querySelectorAll('.arrowLeft')
 const arrowRight = document.querySelectorAll('.arrowRight')
-const
+const menu = document.getElementById("menu")
+
 console.log(arrowLeft)
+
+arrowRight.forEach((items, idx) => {
+    items.addEventListener('click', () => {
+        right()
+    })
+} )
+
+arrowLeft.forEach((items, idx) => {
+    items.addEventListener('click', () => {
+        left()
+    })
+} )
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+    if (e.keyCode == '37') {
+       // left arrow
+        left()
+    }
+    else if (e.keyCode == '39') {
+       // right arrow
+        right()
+    }
+
+}
+
+function left() {
+    if (menu.classList == 'menu firstSlide') {
+        menu.classList.remove('firstSlide')
+    } else if (menu.classList == 'menu firstSlide secondSlide') {
+        menu.classList.remove('secondSlide')
+    }
+}
+
+function right() {
+    if (menu.classList == 'menu') {
+        menu.classList.add('firstSlide')
+    } else if (menu.classList == 'menu firstSlide') {
+        menu.classList.add('secondSlide')
+    } else {
+        menu.classList = 'menu'
+    }
+}
